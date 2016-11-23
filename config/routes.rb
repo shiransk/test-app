b662d1ad79e619d03f69214f31428bb89747e134
Shiran Koriat <shiransk@gmail.com>
2016-11-23 13:49:59 -0800

    add contact us form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/', to: 'pages#home' 
   get '/testing', to: 'pages#testing' 
   get '/about_us', to: 'pages#about_us' 
+  get '/contact_us', to: 'pages#contact_us' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
